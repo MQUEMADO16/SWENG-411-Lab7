3dd5e4bc219edc39a9eb282beeabdfd9facc398d
Quemado <mbq5051@psu.edu>
2025-10-16 09:56:49 -0400

    init commit

---
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,71 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.model.Worker;
+import java.io.*;
+import java.util.ArrayList;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/worker/create")
+    public String createWorker(@PathVariable Model model) throws IOException {
+        Worker[] workerArray = new Worker[5];
+
+        workerArray[0] = new Worker("John", 25, "Male", 3);
+        workerArray[1] = new Worker("Doug", 26, "Male", 4);
+        workerArray[2] = new Worker("Kenny", 27, "Male", 5);
+        workerArray[3] = new Worker("Georgina", 28, "Female", 6);
+        workerArray[4] = new Worker("Katie", 29, "Female", 7);
+
+        File f = new File("Worker.obj");
+        FileOutputStream fos = new FileOutputStream(f);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(workerArray);
+        oos.close();
+        fos.close();
+
+        return "worker";
+    }
+
+    @GetMapping("/worker/{ageValue}")
+    public String workerAge(@PathVariable int ageValue, Model model) throws IOException, ClassNotFoundException {
+
+        FileInputStream fis = new FileInputStream("Worker.obj");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Worker[] workerArray = (Worker[]) ois.readObject();
+        ArrayList<Worker> workersByAge = new ArrayList<>();
+
+        for(Worker w : workerArray) {
+            if (w.getAge() == ageValue) {
+                workersByAge.add(w);
+            }
+        }
+
+        model.addAttribute("workers", workersByAge);
+        model.addAttribute("age", ageValue);
+        return "workerAge";
+    }
+
+    @GetMapping("/worker/sex/{gender}")
+    public String workerGender(@PathVariable String gender, Model model) throws IOException, ClassNotFoundException {
+
+        FileInputStream fis = new FileInputStream("Worker.obj");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Worker[] workerArray = (Worker[]) ois.readObject();
+        ArrayList<Worker> workersByGender = new ArrayList<>();
+
+        for(Worker w : workerArray) {
+            if (w.getGender() == gender) {
+                workersByGender.add(w);
+            }
+        }
+
+        model.addAttribute("workers", workersByGender);
+        model.addAttribute("gender", gender);
+        return "workerGender";
+    }
+}
---
+++ src/main/java/com/example/demo/model/Worker.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+import java.io.*;
+
+public class Worker implements Serializable {
+
+    // Attributes
+    private String name;
+    private int age;
+    private String gender;
+    private int yearsWorkExp;
+
+    // Constructor
+    public Worker(String name, int age, String gender, int yearsWorkExp) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.yearsWorkExp = yearsWorkExp;
+    }
+
+    // Methods
+
+    // Get/Set
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getYearsWorkExp() {
+        return this.yearsWorkExp;
+    }
+
+    public void setYearsWorkExp(int years) {
+        this.yearsWorkExp = years;
+    }
+
+    public String printWorker() {
+        return "Worker: " + name + " | Age: " + age + " | Gender: " + gender + " | Years of Work Experience: " + yearsWorkExp;
+    }
+}
